579f662c78f414a2c3e6d5385c1d04774a70f54f
DanGraziano <dangraziano@outlook.com>
2023-08-17 12:49:46 -0400

    Connect components to app.js page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import TopNavbar from './components/navbar';
-
+import Home from './components/home';
+import Profile from './components/profile';
+import Login from './components/login';
+import Register from './components/register';
+import Search from './components/search';
+import GameDetails from './components/gamedetails';
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
   return (
-      <div className="App">
+      <BrowserRouter>
+        <div className="container border border-dark rounded">
         <TopNavbar />
-        {/* Add other content */}
-      </div>
+          <div className="container border border-dark rounded p-5">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/search" element={<Search/>}/>
+          <Route path="/game/:id" element={<GameDetails />} />
+        </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
   );
 }
+
 export default App;
