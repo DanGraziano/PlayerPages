26d90c4e85666f7914a57476a0a9ad8fa6b9955e
DanGraziano <dangraziano@outlook.com>
2023-08-17 23:18:27 -0400

    Add redux persist

--- src/App.js
+++ src/App.js
@@ -10,24 +10,34 @@ import Search from './components/search';
 import GameDetails from './components/gamedetails';
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import {LoginProvider} from "./context/login-context";
+import {Provider} from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className="container border border-dark rounded">
-        <TopNavbar />
-          <div className="container border border-dark rounded p-5">
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/search" element={<Search/>}/>
-          <Route path="/game/:id" element={<GameDetails />} />
-        </Routes>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <LoginProvider>
+          <div className="container border border-dark rounded">
+          <TopNavbar />
+            <div className="container border border-dark rounded p-5">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/search" element={<Search/>}/>
+            <Route path="/game/:id" element={<GameDetails />} />
+          </Routes>
+            </div>
           </div>
-        </div>
+          </LoginProvider>
       </BrowserRouter>
+        </PersistGate>
+      </Provider>
   );
 }
 
